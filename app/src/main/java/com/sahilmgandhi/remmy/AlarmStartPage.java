0d278a217b2bd58885e65bfb88ab0a5bd68aa69f
Sahil Gandhi <sahilmgandhi@gmail.com>
2016-03-23 16:31:22 -0700

    Third Commit

--- app/src/main/java/com/sahilmgandhi/remmy/AlarmStartPage.java
+++ app/src/main/java/com/sahilmgandhi/remmy/AlarmStartPage.java
@@ -1,38 +1,61 @@
 package com.sahilmgandhi.remmy;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.graphics.PorterDuff;
 
+
+public class AlarmStartPage extends AppCompatActivity
+{
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_start_page);
+
+        final Button remMaker = (Button) findViewById(R.id.remCreateButton);
+        remMaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remMaker.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
+            }
+        });
+    }
+}
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class AlarmStartPage extends AppCompatActivity {
-    /**
+/*public class AlarmStartPage extends AppCompatActivity {
+    *//**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+     *//*
     private static final boolean AUTO_HIDE = true;
 
-    /**
+    *//**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
-     */
+     *//*
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
-    /**
+    *//**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
-     */
+     *//*
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
+    //private View mContentView;
+   private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
@@ -41,12 +64,12 @@ public class AlarmStartPage extends AppCompatActivity {
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
             // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+            *//*mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); *//*
         }
     };
     private View mControlsView;
@@ -68,11 +91,11 @@ public class AlarmStartPage extends AppCompatActivity {
             hide();
         }
     };
-    /**
+    *//**
      * Touch listener to use for in-layout UI controls to delay hiding the
      * system UI. This is to prevent the jarring behavior of controls going away
      * while interacting with activity UI.
-     */
+     *//*
     private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -91,21 +114,21 @@ public class AlarmStartPage extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        //mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
+        //mContentView.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+          //  public void onClick(View view) {
+          //      toggle();
+          //  }
+        //});
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.remCreateButton).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
@@ -143,8 +166,8 @@ public class AlarmStartPage extends AppCompatActivity {
     @SuppressLint("InlinedApi")
     private void show() {
         // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+     *//*   mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);*//*
         mVisible = true;
 
         // Schedule a runnable to display UI elements after a delay
@@ -152,12 +175,12 @@ public class AlarmStartPage extends AppCompatActivity {
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    /**
+    *//**
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
-     */
+     *//*
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
-}
+}*/
